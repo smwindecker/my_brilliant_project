5c8940ec454e240bf492d864200bf965aee5b4ea
Saras Windecker <sm.windecker@unimelb.edu.au>
2019-11-27 14:33:57 +1100

    added the other files

---
+++ analysis.R
@@ -0,0 +1,25 @@
+
+library(remotes)
+library(holepunch)
+
+remotes::install_github("karthik/holepunch")
+
+
+#
+source("R/data_cleaning_functions.R")
+source("R/plotting_functions.R")
+
+# read data
+# decay <- read.csv('data/decay.csv')
+mosquito_data <- read.csv("data/mosquito_data.csv")
+
+# clean and tidies data
+# 
+mosquito_data$salinity_std <- 
+  rescale(mosquito_data$salinity__ppt)
+
+# model
+
+
+# plotting output
+

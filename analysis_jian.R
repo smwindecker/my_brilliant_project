299032fc27a90aa7dae05d2b4f6e7cd72581cb04
Saras Windecker <sm.windecker@unimelb.edu.au>
2019-11-27 14:16:18 +1100

    got rid of that annoying title

--- analysis_jian.R
+++ analysis_jian.R
@@ -208,12 +208,17 @@ plot(plot_values ~ precip_actual,
      ylim = c(0, 1),
      xlab = "Precipitation (mm)",
      ylab = "Probabiliy of occurrence")
-mtext("Species 2", side = 3, line = 0.5, adj = 0.02, cex = 1.5)
 
 # great, but this seems like a huge amount of copy-paste and a lot of
 #   opportunities for mistakes if we want to plot three species by
 #   four predictors. What if we had more species or more predictors?
 
+
+
+
 # I have made a important edit
+
+
+
 
 
